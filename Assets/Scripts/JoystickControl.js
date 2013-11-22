81d7fcf81465d41bef2b080935f24c986be28185
Colin Alteveer <alteveer@gmail.com>
2013-11-22 13:18:33 -0500

    More refactoring, added a player class, player class will get controls and spawn vehicles

--- Assets/Scripts/JoystickControl.js
+++ Assets/Scripts/JoystickControl.js
@@ -1,19 +1,22 @@
 var players : GameObject[];
 
-var player_a_buttons = [
+var a_buttons = new Array(
 	KeyCode.Joystick1Button0,
 	KeyCode.Joystick2Button0,
 	KeyCode.Joystick3Button0,
 	KeyCode.Joystick4Button0
-];
+);
 
 var spawner:PlayerSpawner;
 var controllers:String[];
+var player_prefab:GameObject;
 
 // Use this for initialization
 function Start () {
+	player_prefab = Resources.Load("player");
 	spawner = GetComponent(PlayerSpawner);
 	players = new GameObject[4];
+	CheckControllers();
 	
 	InvokeRepeating("CheckControllers", 0, 1);
 }
@@ -24,33 +27,23 @@ function CheckControllers() {
 
 // Update is called once per frame
 function Update () {
-	for(var k = 0; k < player_a_buttons.Length;k++) {
-		if(Input.GetKeyDown(player_a_buttons[k]) && players[k+1] == null) {
-			var idx = k+1;
-			Debug.Log("Player " + idx + " pressed A");
-			p = spawn_player(idx);
-			if(p) {
-				players[idx] = p;
-				players[idx].GetComponent("input").set_inputs(idx);
+	for(var k = 0; k < a_buttons.length;k++) {
+		if (players[k] == null) {
+			if(Input.GetKeyDown(a_buttons[k])) {		
+				Debug.Log("Player " + (k+1) + " pressed A");
+
+				players[k] = Instantiate(player_prefab, Vector3(), Quaternion.identity);
 			}
 		}
 	}
 }
 
-function spawn_player(player_number:int):GameObject {
-	var open_spawn_location = spawner.get_open_spawn();
-	if (open_spawn_location) {
-		//return Instantiate(tank, open_spawn_location.transform.position, Quaternion.identity);
-	} else {
-		Debug.Log("No free spawns");
-	}
-	
-}
+
 
 function OnGUI () {
 	for (var i = 0; i < players.Length; i++) {
 
-		GUILayout.Label("Player " + (i+1) + ": " + 
+		GUILayout.Label("Player " + (i+1) + ": " + players[i] + " -- " +
 			((controllers.Length > i) ? controllers[i] : "Please connect a controller."));
 	}	
 
---
+++ Assets/Scripts/Player.js
@@ -0,0 +1,34 @@
+﻿#pragma strict
+
+var _controller_index:int = -1;
+var _vehicle_reference:GameObject;
+
+var LSTICK_X;
+var LSTICK_Y;
+var RSTICK_X;
+var RSTICK_Y;
+var RTRIGGER;
+var FIRE_PRIMARY : KeyCode;
+var FIRE_SECONDARY : KeyCode;
+
+function set_controller_index(index:int):void {
+	_controller_index = index;
+	LSTICK_X = "JOY" + index + "_LSTICK_X";
+	LSTICK_Y = "JOY" + index + "_LSTICK_Y";
+	RSTICK_X = "JOY" + index + "_RSTICK_X";
+	RSTICK_Y = "JOY" + index + "_RSTICK_Y";
+	FIRE_PRIMARY = KeyCode.Parse(KeyCode, "Joystick"+index+"Button5");
+	FIRE_SECONDARY = KeyCode.Parse(KeyCode, "Joystick"+index+"Button4");
+}
+
+function get_controller_index():int {
+	return _controller_index;
+}
+
+function Start () {
+
+}
+
+function Update () {
+
+}
